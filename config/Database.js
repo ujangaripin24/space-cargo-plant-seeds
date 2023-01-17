92f25994f4d09eda7bf0c369a2e1e6e0b19a7226
ujangaripin24 <ujangarifin06@gmail.com>
2023-01-17 19:52:45 +0700

    fix system auth

--- config/Database.js
+++ config/Database.js
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 
 const db = new Sequelize('nodejs_proyek_14', 'necronomicon', '123456789', {
     host: "localhost",
-    dialect: "pgsql"
+    dialect: "postgres"
 });
 
 export default db;
